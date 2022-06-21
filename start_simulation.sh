2a282deea5b84b4956d3b45cee889d55d1e335e0
RyosukeMatsushima <m.ryosuke@me.com>
2022-06-21 14:49:51 +0900

    add script for starting simulation with mavros

---
+++ start_simulation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
+
+roslaunch px4 posix_sitl.launch world:=$(pwd)/Tools/sitl_gazebo/worlds/warehouse.world
+
